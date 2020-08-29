f69e11bb3b30d08fe0a8be36101b6d24aab9aca2
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2020-08-29 16:23:47 +0100

    Update Lab8_FrontOfQueue.java

--- Queues/Lab8_FrontOfQueue.java
+++ Queues/Lab8_FrontOfQueue.java
@@ -5,53 +5,41 @@ package Previous_Labs;
 */
 import java.util.Scanner;
 
-public class Lab8_FrontOfQueue
-{
-    public static void main(String args[])
-    {
+public class Lab8_FrontOfQueue {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
        
         // The queue will never be bigger than 20
         Queue myQueue = new Queue(20); 
-		while(sc.hasNext())
-		{		
+		while(sc.hasNext()) {		
 		// Input when it reaches a space and puts each element in an array
 	        String arr[] = sc.nextLine().split(" "); 
 	
 	        //You need to put in a stopping condition when using standard input with hasNext()
-	        if(arr[0].equals("STOP")) 
-	        {
-	        	break;
+	        if(arr[0].equals("STOP")) {
+			break;
 	        }
 	
 	        // If string is not "STOP" and array element is 0, insert "INSERT"
-	        else if(arr[0].equals("INSERT"))
-	        {
+	        else if(arr[0].equals("INSERT")) {
 	            myQueue.insert(arr[1]);
-	        }
-	
-	        else // If it says REMOVE
-	        {	
-	            if(myQueue.isEmpty())
-	            {
+	        } else {// If it says REMOVE
+	      	
+	            if(myQueue.isEmpty()) {
 	            	// Will move on to the next iteration of the loop if the stack's empty
 	                continue; 
-	            }
-
-	            else
-	            {
+	            } else {
 	            	 myQueue.remove();
 	            }
 	        }
-	        sc.close();
+	        	sc.close();
 	                   
 		}
 		System.out.println(myQueue.remove());
     }
 }
 
-class Queue
-{
+class Queue {
 	private int maxSize;
 	private String[] queueArray;
 	private int front;
@@ -59,8 +47,7 @@ class Queue
 	private int nItems;
 
 	// Constructor
-	public Queue(int size)
-	{ 		
+	public Queue(int size) { 		
 		maxSize = size;
 		queueArray = new String[size];
 		front = 0;
@@ -69,14 +56,12 @@ class Queue
 	}
 	
 	// Inserts an element at the rear of the queue
-	public boolean insert(String x)
-	{ 
+	public boolean insert(String x) { 
 		if(isFull())
 			return false;
 		
 		// If the back of the queue is the end of the array wrap around to the front
-		if(rear == maxSize-1) 
-		{
+		if(rear == maxSize-1) {
 			rear = -1;
 		}
 
@@ -87,10 +72,8 @@ class Queue
 	}
 	
 	// Remove an element from the front of the queue
-	public String remove()
-	{ 
-		if(isEmpty())
-		{
+	public String remove() { 
+		if(isEmpty()) {
 			System.out.println("Queue is empty");
 			return null;
 		}
@@ -108,14 +91,12 @@ class Queue
 	}
 
 	// Returns true is the queue is full
-	public boolean isFull()
-	{
+	public boolean isFull() {
 		return(nItems == maxSize);
 	}
 
 	// Returns true is the queue is empty
-	public boolean isEmpty()
-	{ 
+	public boolean isEmpty() { 
 		return(nItems == 0);
 	}
 }
