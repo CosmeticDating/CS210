565e1ce76fbfb5bb0ecdf0e35e76a72e26d10380
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:29:40 +0000

    Update LinkedList_practice.java

--- Linked_Lists/LinkedList_practice.java
+++ Linked_Lists/LinkedList_practice.java
@@ -1,7 +1,6 @@
-public static int findLoopLength(LinkedList myList)
-{
+public static int findLoopLength(LinkedList myList) {
 	//If there is nothing in myList, return nothing
-	if(myList.isEmpty){
+	if(myList.isEmpty) {
 		return 0;
 	}
 	//start at the beginning
@@ -14,16 +13,13 @@ public static int findLoopLength(LinkedList myList)
 	count = 0;
 	
 	//while the current link has information in it
-	while(current.next != null)
-	{	//set the array
+	while(current.next != null) {	//set the array
 		arr[count] = current;
 		
 		//search for a link with the same value to see if we are in a loop or not
-		for(int i = 0; i < count; i++)
-		{   
+		for(int i = 0; i < count; i++) {   
 			//if we found it
-			if(arr[i] == current)
-			{
+			if(arr[i] == current) {
 				return count - i;
 			}
 			//keep counting updates
@@ -38,51 +34,19 @@ public static int findLoopLength(LinkedList myList)
 }
 	
 	
-public Link Delete(int x)
-{
+public Link Delete(int x) {
 	Link current = head;
-	
-	while(current.value != x)
-	{
+	while(current.value != x) {
 		current = current.next;
 	}
-	
-	if(current == head)
-	{
+	if(current == head) {
 		deleteHead();
 	}
-	
-	if(current == tail)
-	{
+	if(current == tail) {
 		deleteTail();
-	}
-	
-	else()
-	{
+	} else {
 		current.next.previous = current.previous;
 		current.previous.next = current.next;
 	}
-	
 	return current;
 }
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
