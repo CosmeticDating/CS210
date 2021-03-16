65cc5ef28f3e87aabd3620e7e07d1658784eb6c4
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:55:37 +0000

    Update Insertion.java

--- Sorting Algorithms Alone/Insertion.java
+++ Sorting Algorithms Alone/Insertion.java
@@ -3,34 +3,28 @@ package Sorting;
 import java.util.Arrays;
 
 //Silly names given to aid in memorisation.  harry = array, hot = record and cold = temp
-public class Insertion
-{
-  public static void main(String args[])
-  {
-    int[] harry = {1, 65, 8, 3, 18, 15, 53};
+public class Insertion {
+  public static void main(String args[]) {
+    int[] arr = {1, 65, 8, 3, 18, 15, 53};
 
-    //Cycle through the array beginning at element 1
-    for(int i = 1; i < harry.length; i++)
-    { 	
-    	int hot = i;
+    // Iterate through the array beginning at element 1
+    for(int i = 1; i < arr.length; i++) { 	
+    	int selection = i;
     	
-    	//Cold is the temporary backup
-    	int cold = harry[i];
+    	// Value held and stored outside of array temporarily
+    	int temp = arr[i];
 	
-	    	//Loop to shift elements to make space
-	    	while((hot > 0) && harry[hot - 1] >= cold)
-	    	{
-	    		//shift elements up to make room for insertion
-	    		harry[hot] = harry[hot - 1];
+	    	// Loop to shift elements to make space
+	    	while((selection > 0) && arr[selection - 1] >= temp) {
+	    		// Shift array elements up one slot to make room for insertion
+	    		arr[selection] = arr[selection - 1];
 	    		
-	    		//Move downwards
-	    		hot--;
+	    		// Move downwards
+	    		selection--;
 	    	}
-	    //Make the temp value the element value
-    	harry[hot] = cold;
+        //Make the temp value the element value
+    	arr[selection] = temp;
     }
-    
-    System.out.println(Arrays.toString(harry));
+    System.out.println(Arrays.toString(arr));
   }
-
 }
