565a731e072485ca2e14238b34cf5e243fbff0db
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 02:04:01 +0000

    Update Brackets2.java

--- Stacks/Brackets2.java
+++ Stacks/Brackets2.java
@@ -5,22 +5,19 @@ package Stacks;
 **/
 import java.util.Scanner;
 
-public class Brackets2
-{
-	   public static boolean isBalanced(String ex) 
-       {
-          //Use length to find the length of the string
-          int stringLength = ex.length();
+public class Brackets2 {
+	   public static boolean isBalanced(String ex) {
+	       //Use length to find the length of the string
+	       int stringLength = ex.length();
 
-           //If the string has no length and expression(ex) isn't anything return true.
-           //Zero is considered balanced
+	       // If the string has no length and expression(ex) isn't anything return true.
+	       // Zero is considered balanced
 	       if(stringLength == 0 || ex == null) return true;
 	    
 	       //Create a stack for the bracket characters
 	       java.util.Stack<Character> stack = new java.util.Stack<> ();
 	      
-	    for(int i = 0; i < ex.length(); i++)
-	    {
+	    for(int i = 0; i < ex.length(); i++) {
 	    	//If ( or [ or { is typed in, push
 	        if(ex.charAt(i) == '(' || ex.charAt(i)=='[' || ex.charAt(i)=='{')  stack.push(ex.charAt(i));
 	        
@@ -36,27 +33,22 @@ public class Brackets2
 	        //else boolean is set to false, unbalanced number of brackets
 	        else return false;
 	    }
-	    
 	    return stack.empty();
 	}	
 	
-	    public static void main(String[] args) 
-	    {
-
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		    
+
 		//Type in the number of bracket strings
 		int N = in.nextInt();
 
-           for (int i = 0; i < N; i++) 
-           {
-               //type in the string expression
-               String expression = in.next();
+		for (int i = 0; i < N; i++) {
+			//type in the string expression
+			String expression = in.next();
 
-               //Method boolean isBalanced determines if the answer is YES or NO.
-               System.out.println( (isBalanced(expression)) ? "YES" : "NO" );
-           }
-          in.close(); 
-	}
-  	    
+			//Method boolean isBalanced determines if the answer is YES or NO.
+			System.out.println( (isBalanced(expression)) ? "YES" : "NO" );
+		}
+		in.close(); 
+	}   
 }
