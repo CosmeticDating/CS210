819b353087b41db2f4fc420ff59909e9d37ccd83
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:04:29 +0000

    Update Lab10B2_Largest_Possible_int.java

--- Byte Manipulation/Lab10B2_Largest_Possible_int.java
+++ Byte Manipulation/Lab10B2_Largest_Possible_int.java
@@ -25,41 +25,28 @@ Sample output
 (courtesy of Manyiu Lam)
 **/
 
-public class Lab10B2_Largest_Possible_int
-{
-    public static void main(String args[] ) throws Exception 
-    {
+public class Lab10B2_Largest_Possible_int {
+    public static void main(String args[] ) throws Exception {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt(), b = in.nextInt(), k = Integer.MIN_VALUE;
 
         if((a & b) > k) k =  (a & b);
-
         if((a ^ b) > k) k =  (a ^ b);
-
         if((a | b) > k) k =  (a | b);
-
         if(~(a & b) > k) k = ~(a & b);
-
         if(~(a ^ b) > k) k = ~(a ^ b);
-
         if(~(a | b) > k) k = ~(a | b);
-
+        
         if(~(b & a) > k) k = ~(b & a);
-
         if(~(b ^ a) > k) k = ~(b ^ a);
-
         if(~(b | a) > k) k = ~(b | a);
-
+        
         if((~a & b) > k) k = (~a & b);
-
         if((~a ^ b) > k) k = (~a ^ b);
-
         if((~a | b) > k) k = (~a | b);
 
         if((~b & a) > k) k = (~b & a);
-
         if((~b ^ a) > k) k = (~b ^ a);
-
         if((~b | a) > k) k = (~b | a);
 
         System.out.print(k);
