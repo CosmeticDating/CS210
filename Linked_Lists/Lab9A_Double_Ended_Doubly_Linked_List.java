5b0d5498c7392883457273ca48e206c5c97bdd4d
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:19:34 +0000

    Update Lab9A_Double_Ended_Doubly_Linked_List.java

--- Linked_Lists/Lab9A_Double_Ended_Doubly_Linked_List.java
+++ Linked_Lists/Lab9A_Double_Ended_Doubly_Linked_List.java
@@ -15,86 +15,66 @@ There is no input. The linked list is created automatically.
 Output Format
 Output the largest value held in any of the links. If the linked list is empty or defective output 0.
 **/
-public class Lab9A_Double_Ended_Doubly_Linked_List
-{
-	public static int search(DoublyLinkedList mylist)
-	{
+public class Lab9A_Double_Ended_Doubly_Linked_List {
+	public static int search(DoublyLinkedList mylist) {
 
 	    /* return the highest value held in any of the links, 0 if defective or empty*/
 	    //if the list is empty or the list is defective, return 0
-	    if(mylist.isEmpty())
-	    {
+	    if(mylist.isEmpty()) {
 	        return 0;
 	    } 
 	    
 	    Link current = mylist.first;
-
 	    int highest = mylist.first.data;
 
-	    while(current.next != null)
-	    {
-	        if(current.next.previous == current)
-	        {	       
+	    while(current.next != null) {
+	        if(current.next.previous == current) {	       
 	        	//search through the linked list
-	            if( highest < current.data)
-	            {
+	            if (highest < current.data) {
 	                highest = current.data;
 	            }
 
-	        }
-	            else
-	            {
-	                return 0;
-	            }
-	        
+	        } else {
+	        	return 0;
+	    	}
 	        current = current.next;
 	    } 
-	    
 	    return highest;
 	}
     
-    class Link
-    {
+    class Link {
     	public int data;
     	public Link next;
     	public Link previous;
 
-    	public Link(int input)
-    	{
+    	public Link(int input) {
     		data = input;
     	}
     }//end class Link
 
-    
-    class DoublyLinkedList 
-    {
+    class DoublyLinkedList {
     	public Link first; //ref to first item
     	public Link last;  //ref to last item
 
     	//Before inserts, the first and last values are null
-    	public DoublyLinkedList( )
-    	{
+    	public DoublyLinkedList() {
     		first = null;
     		last = null;
     	}
 
     	//If empty the first node is equal to null value
-    	public boolean isEmpty( )
-    	{
+    	public boolean isEmpty() {
     		return (first == null);
     	}
 
     	//If the first node is empty, insert value.  Else, insert into the next node
-    	public void insertHead(Link insert)
-    	{
+    	public void insertHead(Link insert) {
     		if(isEmpty())
     			first = insert;
-
-    		else
-    		{
+    		else {
     			insert.next = first;
     			first = insert;
     		}
     	}
     }
-}
+} 
