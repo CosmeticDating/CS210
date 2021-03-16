9da7a1b0e7e893513c2065f7558725faf11d5ef8
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:56:28 +0000

    Update Insertion_Shedford.java

--- Sorting Algorithms Alone/Insertion_Shedford.java
+++ Sorting Algorithms Alone/Insertion_Shedford.java
@@ -1,10 +1,8 @@
 package Sorting;
 
-public class Insertion_Shedford 
-{
+public class Insertion_Shedford {
 	//Example
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		int arr[] = {3, 1, 56, 7, 21, 2};
 		arr = insertion(arr);
 		for(int i = 0; i < arr.length; i++)
@@ -12,15 +10,15 @@ public class Insertion_Shedford
 	}
 	
 	//Insertion sort method
-	public static int[] insertion(int[] arr) //ascending order
-	{ 
-		for(int i = 1; i < arr.length; i++){
+	public static int[] insertion(int[] arr) //ascending order { 
+		for(int i = 1; i < arr.length; i++) {
 			int record = i;
 			int temp = arr[i]; //Back up the current element
-			for(int j = i; j > 0; j--) //Work back from the current element
-				if(arr[j - 1] >= temp){
+			for(int j = i; j > 0; j--) { //Work back from the current element
+				if(arr[j - 1] >= temp) {
 					record = j - 1; //Keep track of the best position found so far to place current element
 					arr[j] = arr[j - 1]; //Shift elements up to make room for insertion
+				}
 			}
 			arr[record] = temp; //Place the current element in the best position found
 		}
