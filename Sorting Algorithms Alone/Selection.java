b23744c1e7d3492525856d42b882057428617e00
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 02:00:49 +0000

    Update Selection.java

--- Sorting Algorithms Alone/Selection.java
+++ Sorting Algorithms Alone/Selection.java
@@ -2,23 +2,20 @@ package Sorting;
 
 import java.util.Arrays;
 
-public class Selection
-{
+public class Selection {
 	//Selection sort method
-	public static int[] selection(int[] arr)
-	{ //ascending order
+	public static int[] selection(int[] arr) { //ascending order
 
-		for(int i = 0; i < arr.length; i++)
-		{
+		for(int i = 0; i < arr.length; i++) {
 			int record = i;
 			
 			//Iterate from the current element to the end of the array
-			for(int j = i; j < arr.length; j++) 
-				if(arr[j] < arr[record])
-				{
+			for(int j = i; j < arr.length; j++) { 
+				if(arr[j] < arr[record]) {
 					//Keep a track of the smallest number found so far	
 					record = j; 				
 				}
+			}
 			
 			//Swap the current element with the smallest number found
 			int temp = arr[i];
@@ -27,12 +24,10 @@ public class Selection
 		}
 		return arr;
 	}
-	//Example
-	public static void main(String args[])
-	{
+	
+	public static void main(String args[]) {
 		int arr[] = {3, 1, 56, 7, 13, 21, 2};
 		arr = selection(arr);
-
 		System.out.print(Arrays.toString(arr)); // 1 2 3 7 21 56
 	}
 }
