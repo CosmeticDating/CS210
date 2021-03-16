2dd5ee17f9919cfd489c26a7dad6159236d33492
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 02:17:38 +0000

    Update Stack_In_Reverse_util.java

--- Stacks/Stack_In_Reverse_util.java
+++ Stacks/Stack_In_Reverse_util.java
@@ -1,22 +1,16 @@
 package Stacks;
 import java.util.Stack;
 
-public class Stack_In_Reverse_util
-{
+public class Stack_In_Reverse_util {
 	//Using Stack class for stack implementation
 	static Stack<String> st = new Stack<>();
 
 	// Below is a recursive function that inserts an element
 	// at the bottom of a stack.
-	static void insert_at_bottom(String x)
-	{	
-		if(st.isEmpty())
-		{
+	static void insert_at_bottom(String x) {	
+		if(st.isEmpty()) {
 			st.push(x);			
-		}
-
-		else
-		{
+		} else {
 			/* All items are held in Function Call Stack until we
 			reach end of the stack. When the stack becomes
 			empty, the st.size() becomes 0, the
@@ -35,10 +29,8 @@ public class Stack_In_Reverse_util
 	
 	// Below is the function that reverses the given stack using
 	// insert_at_bottom()
-	static void reverse()
-	{
-		if(st.size() > 0)
-		{
+	static void reverse() {
+		if(st.size() > 0) {
 			/* Hold all items in Function Call Stack until we
 			reach end of the stack */
 			String x = st.peek();
@@ -54,8 +46,7 @@ public class Stack_In_Reverse_util
 	}	
 
 	// Driver method
-	public static void main(String[] args) 
-	{			
+	public static void main(String[] args) {			
 		String D = "North South East West";
 		
 		//Push words from the string into the stack string
@@ -65,15 +56,12 @@ public class Stack_In_Reverse_util
 		st.push(D.substring(17,21));
 		
 		System.out.println("Directions:");
-		
 		System.out.println(st);
 		
 		//Function to reverse the stack
 		reverse();
 		
 		System.out.println("\nDirections reversed:");
-		
 		System.out.println(st);
 	}
-
 }
