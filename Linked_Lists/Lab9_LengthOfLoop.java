1816d6b40616aef5725808c1cd11e6718f491c04
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:24:02 +0000

    Update Lab9_LengthOfLoop.java

--- Linked_Lists/Lab9_LengthOfLoop.java
+++ Linked_Lists/Lab9_LengthOfLoop.java
@@ -7,55 +7,42 @@ package Previous_Labs;
 
 import java.util.Scanner;
 
-public class Lab9_LengthOfLoop 
-{
-	public static void main(String args[] ) throws Exception 
-	{
+public class Lab9_LengthOfLoop {
+	public static void main(String args[] ) throws Exception {
 		Scanner sc = new Scanner(System.in);
  		int num = Integer.parseInt(sc.nextLine());
  		Link[] array = new Link[num];
 
- 		for(int i = 0; i < num; i++)
- 		{
+ 		for(int i = 0; i < num; i++) {
  			array[i] = new Link(sc.nextLine());
  		}
 
- 		while(sc.hasNext())
- 		{
+ 		while(sc.hasNext()) {
  			int select = sc.nextInt();
  			
  			//Need a stopping condition when using hasNext() with standard input
- 			if(select == -1) 
- 			{
+ 			if(select == -1) {
  				break;
  			}
 
  			int next = sc.nextInt();
- 			if(next != -1)
- 			{
+ 			if(next != -1) {
  				array[select].next=array[next];
  			}
-
  		}
  		
  		LinkedList mylist = new LinkedList();
- 		if(num > 0)
- 		{
+ 		if(num > 0) {
  			mylist.first = array[0];
  		}
-
-
  		System.out.println(findLoopLength(mylist));
- 		
  		sc.close();
 	}
 
 	//This is the method we had to write
- 	public static int findLoopLength(LinkedList myList)
- 	{
+ 	public static int findLoopLength(LinkedList myList) {
  		//Return 0 if the list is empty
-        if(myList.isEmpty())
-        {
+        if(myList.isEmpty()) {
             return 0; 
         }
 
@@ -68,21 +55,16 @@ public class Lab9_LengthOfLoop
         Link arr[] = new Link[100]; 
 
         //While the list is not pointing to null
-        while(current.next != null)
-        {
+        while(current.next != null) {
         	//Populate the array with the links from the list
             arr[count] = current; 
 
             //Check if we have seen the current element before(i.e if it's in the array)
-            for(int i = 0; i < count; i++)
-            {
-            	
-                if(arr[i] == current)
-                {
-                	//The length of the loop is the index of current minus the index of the repeated element
+            for(int i = 0; i < count; i++) {
+                if(arr[i] == current) {
+                    //The length of the loop is the index of current minus the index of the repeated element
                     return count - i;                	
                 }           
-
 	            //Increment counter for each iteration of the loop
 	            count++; 
 	            
@@ -96,42 +78,35 @@ public class Lab9_LengthOfLoop
    }
 }
 
-class Link
-{
+class Link {
 	public String data;
 	public Link next;
-
-	public Link(String input)
-	{
+	
+	public Link(String input) {
 		data = input;
 		next = null;
 	}
 }
 
-class LinkedList 
-{
+class LinkedList  {
 	public Link first;
 
 	//Before inserts, the first value is null
-	public LinkedList( )
-	{
+	public LinkedList() {
 		first = null;
 	}
 
 	//If empty the first node is equal to null value
-	public boolean isEmpty( )
-	{
+	public boolean isEmpty() {
 		return (first == null);
 	}
 
 	//If the first node is empty, insert value.  Else, insert into the next node
-	public void insertHead(Link insert)
-	{
+	public void insertHead(Link insert) {
 		if(isEmpty())
 			first = insert;
 
-		else
-		{
+		else {
 			insert.next = first;
 			first = insert;
 		}
