837309548332f59559d6e1bc43bf6ced0e0e18ae
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:32:16 +0000

    Update Linked_List_Banas.java

--- Linked_Lists/Linked_List_Banas.java
+++ Linked_Lists/Linked_List_Banas.java
@@ -1,126 +1,89 @@
-public static void main(String args[])
-{
+public static void main(String args[]) {
 	LinkList theLinkedList = new LinkList();
-	
 	LinkList.insertFirstLink("Don Quixote", 500);
 	LinkList.insertFirstLink("A Tale of Two Cities", 200);
 	LinkList.insertFirstLink("The Lord of the Rings", 150);
 	LinkList.insertFirstLink("Harry Potter and the Sorceror's Stone", 107);
 
 	theLinkedList.removeFirst();
-
 	theLinkedList.display();
 	
-	System.out.println(theLinkedList.find("The Lord of the Rings").bookName
-	+ " was found.");
-	
+	System.out.println(theLinkedList.find("The Lord of the Rings").bookName + " was found.");
 	theLinkedList.removeLink("The Lord of the Rings");
-	
 	theLinkedList.display();
 }
 
-public class Link 
-{
+public class Link {
 	public String bookName;
 	public int millionsSold;
 	
 	public Link next;
 	
-	public Link(String bookName, int millionsSold)
-	{
+	public Link(String bookName, int millionsSold) {
 		this.bookName = bookName;
 		this.millionsSold = millionsSold;
 	}
 	
-	public void display()
-	{
+	public void display() {
 		System.out.println(bookName + ": " + millionsSold + ",000,000");
 	}
 
-	class LinkList
-	{
+	class LinkList {
 		public Link firstLink;
 		
-		LinkList()
-		{
+		LinkList() {
 			firstLink = null;
 		}
 		
-		public boolean isEmpty()
-		{
+		public boolean isEmpty() {
 			return(firstLink == null);
 		}
 		
-		public void insertFirstLink(String bookName, int millionsSold)
-		{
-			Link newLink = new Link(bookName, millionsSold)
-			{
+		public void insertFirstLink(String bookName, int millionsSold) {
+			Link newLink = new Link(bookName, millionsSold) {
 				newLink.next = firstLink;
 				firstLink = newLink;
 			}
 		}
 		
-		public void removeFirst()
-		{
+		public void removeFirst() {
 			Link Linkreference = firstLink;
 			
-			if(!isEmpty())
-			{
+			if(!isEmpty()) {
 				firstLink = firstLink.next;
-			}
-			else
-			{
+			} else {
 				System.out.println("Empty LinkedList");
 			}
 		}
-		
 		return linkReference;
 	}
 	
-	public void display()
-	{
+	public void display() {
 		Link theLink = firstLink;
-		
-		while(theLink != null)
-		{
+		while(theLink != null) {
 			theLink.display();
-			
 			System.out.println("Next Link: " + theLink.next);
-			
 			theLink = theLink.next;
 		}
 	}
 	
-	public Link removeLink(String bookName)
-	{
+	public Link removeLink(String bookName) {
 		Link currentLink = firstLink;
 		Link previousLink = firstLink;
 		
-		while(currentLink.bookName != bookName)
-		{
-			if(currentLink.next == null)
-			{
+		while(currentLink.bookName != bookName) {
+			if(currentLink.next == null) {
 				return null;
-			}
-			else
-			{
+			} else {
 				previousLink = current;
 				currentLink = currentLink.next;
-			}
-				
+			}		
 		}
-		
-		if(currentLink == firstLink)
-		{
+		if(currentLink == firstLink) {
 			firstLink = firstLink.next;
-		}
-		else
-		{
+		} else {
 			previousLink.next = currentLink.next;
 		}
-		
-		return currentLink;
-			
+		return currentLink;	
 	}
-	
 }
