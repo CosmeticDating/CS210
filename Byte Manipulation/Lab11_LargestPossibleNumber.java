fa33d1cfdbd9c960414249edb027f276978f2904
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:07:54 +0000

    Update Lab11_LargestPossibleNumber.java

--- Byte Manipulation/Lab11_LargestPossibleNumber.java
+++ Byte Manipulation/Lab11_LargestPossibleNumber.java
@@ -3,14 +3,13 @@ package Previous_Labs;
 //This is Phil's Solution
 import java.util.*;
 public class Lab11_LargestPossibleNumber{
- public static void main(String args[]) throws Exception 
- {
+   public static void main(String args[]) throws Exception {
 	 Scanner myscanner = new Scanner(System.in);
 	 int num1 = myscanner.nextInt();
 	 int num2 = myscanner.nextInt();
 	 int record=Integer.MIN_VALUE;
- 	 for(int i = 0;i<=32;i++){
- 		 for(int j=0;j<=32;j++){
+ 	 for(int i = 0;i<=32;i++) {
+ 		 for(int j=0;j<=32;j++) {
  			 if(((num1<<i)|(num2<<j))>record){
  				 record=((num1<<i)|(num2<<j));
  			 }
@@ -27,5 +26,5 @@ public class Lab11_LargestPossibleNumber{
  	 }
 	 System.out.println(record);
 //This is just brute force - try everything, no thinking required
- }
+   }
 }
