cb476be09d0c3eda36543b865089cbe595cfd88f
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:49:58 +0000

    Update Bubble_Sort.java

--- Sorting Algorithms Alone/Bubble_Sort.java
+++ Sorting Algorithms Alone/Bubble_Sort.java
@@ -2,34 +2,26 @@ package Sorting;
 
 import java.util.Arrays;
 
-public class Bubble_Sort
-{
-  public static void main(String args[])
-  {
+public class Bubble_Sort {
+  public static void main(String args[]) {
     int[] array = {13, 5, 8, 3, 10, 15, 53};
-
     boolean swapped = false;
     
-    do
-    {
+    do {
       swapped = false;
 
-      for(int i = 0; i < array.length - 1; i++)
-      {
-        if(array[i] > array[i + 1])
-        {
+      for(int i = 0; i < array.length - 1; i++) {
+        if(array[i] > array[i + 1]) {
           int temp = array[i];
           array[i] = array[i + 1];
           array[i + 1] = temp;
-
-          swapped = true;
+          // Keep iterating until the if statement setting swapped to true no longer occurs
+          swapped = true; 
         }
       } 
     } 
     while(swapped);
-
     System.out.println(Arrays.toString(array));
-
   }
 
 }
