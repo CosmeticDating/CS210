04d989ce3d1d2f7f318cb29bd5e42186c186352c
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2021-03-16 01:46:00 +0000

    Update Lab6_SortWords.java

--- N Squared Sorting Algorithms/Lab6_SortWords.java
+++ N Squared Sorting Algorithms/Lab6_SortWords.java
@@ -4,12 +4,10 @@ package Previous_Labs;
 //are the same size, order them in alphabetical order.
 
 import java.util.Scanner;
-public class Lab6_SortWords
-{
+public class Lab6_SortWords {
 	private static Scanner sc;
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		sc = new Scanner(System.in);
 
 		//input the size of the array and first name
@@ -17,25 +15,21 @@ public class Lab6_SortWords
 		String arr[] = new String[n];
 
 		//input names
-		for(int i = 0; i < n; i++)
+		for(int i = 0; i < n; i++) {
 			arr[i] = sc.nextLine();
-
+		}
 		//Insertion sort
-		for(int i = 0; i < n; i++)
-		{ 
+		for(int i = 0; i < n; i++) { 
 			//Give record an initial value of i, being 0
 			int record = i;
 			
 			//for loop search to track smallest
-			for(int j = i; j < n; j++)
-			{
-				if(arr[j].length() < arr[record].length())
-					
+			for(int j = i; j < n; j++) {
+				if(arr[j].length() < arr[record].length()) {
 					//smallest found held in element j
-					record = j; 
+					record = j; 					
+				}
 			}
-			
-			
 			//make a temp duplicate of i
 			String temp = arr[i];
 			
@@ -46,11 +40,9 @@ public class Lab6_SortWords
 			arr[record] = temp; 
 
 			//If two words are the same length, put them in alphabetical order
-			if(i > 0 && arr[i].length() == arr[i - 1].length())
-			{ 
+			if(i > 0 && arr[i].length() == arr[i - 1].length()) { 
 				//Compare to used to sort alphabetically 
-				if(arr[i].compareTo(arr[i - 1]) < 0)
-				{
+				if(arr[i].compareTo(arr[i - 1]) < 0) {
 					//Swapping values using a temp
 					String x = arr[i - 1];
 					arr[i - 1] = arr[i];
@@ -60,8 +52,7 @@ public class Lab6_SortWords
 		}
 		
 		//Print out the sorted list
-		for(int i = 0; i < n; i++)
-		{
+		for(int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
 		}
 			
